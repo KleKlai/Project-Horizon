481c3188fe6e00ec84f212a9d48b871572151af1
kleklai <mmagallen@klaiklai.tech>
2020-04-14 01:09:20 +0800

    bash script hot fix

--- install.sh
+++ install.sh
@@ -10,13 +10,14 @@ echo "======== Installation Finish ========"
 read -p "Do you already add database credentials in your .env file? [y|n]?" CONT
 if [ "$CONT" = "y" ]; then
   php artisan migrate:fresh --seed
+
+  read -p "Do you want to serve? [y|n]?" CONT
+  if [ "$CONT" = "y" ]; then
+    php artisan serve
+  else
+    echo "Project is now development ready..."
+    echo "======== Happy Coding... ========"
+  fi
 else
   echo "Kindly add your database credentials in env file."
-fi
-
-read -p "Do you want to serve? [y|n]?" CONT
-if [ "$CONT" = "y" ]; then
-  php artisan serve
-else
-  echo "Project is now development ready..."
 fi
